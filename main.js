f24cb7e84caa3a4310e8d27b5f9c7fbac3561a0c
gerich <gkaraulanovs2004@gmail.com>
2020-10-09 18:33:00 +0300

    main

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const settings = require('./front/settings.json')
-
+const Porcupine = require("@picovoice/porcupine-node");
 
 if(settings.name === "undefiened"){
     console.log("Change name for your home assiastant!")

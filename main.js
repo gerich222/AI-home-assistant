d1f86b81f3b03464dfc729447d61c11ab2949fd9
gerich <gkaraulanovs2004@gmail.com>
2020-10-09 18:00:25 +0300

    0.0.2

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const settings = require('./front/settings.json')
 
+
 if(settings.name === "undefiened"){
     console.log("Change name for your home assiastant!")
     const serv = require('./server.js')
